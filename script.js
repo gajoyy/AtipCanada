adc742012d32973466a4bdcb322cc1bcc7907680
ebagabe <gabriel.ebagabe@gmail.com>
2024-06-05 12:30:47 -0400

    Finalizando MVP

--- script.js
+++ script.js
@@ -54,10 +54,16 @@ function updateDropzoneText(dropzoneText, files) {
 const menuButton = document.getElementById("menu-hamburguer");
 const menu = document.getElementById("menu");
 
-menuButton.addEventListener("click", function() {
-  if (menu.style.display === "block") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "block";
-  }
+menuButton.addEventListener("click", function () {
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+});
+
+window.addEventListener("resize", function () {
+    if (window.innerWidth > 768) { 
+        menu.style.display = "none";
+    }
 });
